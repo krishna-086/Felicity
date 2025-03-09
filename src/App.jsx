ed985ba010b500e33341b8a7e6f768b33752f52e
Ashna <manowarashna@gmail.com>
2025-03-10 03:40:17 +0530

    Courses page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ContactPage from "./pages/Contact";
 import AboutUs from "./pages/About";
-
+import Course from "./pages/Course";
 
 const App = () => {
   return (
     <Router>
       <div className="w-full min-h-screen">
-        <Navbar />
-       <Routes>
+        
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact-us" element={<ContactPage />} />
           <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/computerscience/datastructure" element={<Course />} />
         </Routes>
       </div>
     </Router>
