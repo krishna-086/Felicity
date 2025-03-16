faa68fc77c930c115f1f74593d3a457bbb32de6a
Mohak-Singhal <mohaksinghal516@gmail.com>
2025-03-16 10:13:17 +0530

    fix: nav toggle issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,6 +50,7 @@ const Navbar = () => {
         target.scrollIntoView({ behavior: "smooth" });
       }
     }
+    setIsOpen(false);
   };
 
   return (
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,16 +9,12 @@ const AboutUs = () => {
     <Navbar/>
     <div className="min-h-screen w-screen flex flex-col items-center justify-center bg-white   mt-15">
       <div className="w-[] shadow-lg rounded-lg p-8 md:p-12">
-        <nav className="text-sm  text-gray-500 mb-6">
-          <span className="hover:text-blue-500 cursor-pointer">Home</span> » 
-          <span className="text-blue-500"> About Us</span>
-        </nav>
-        <h1 className="text-4xl font-bold text-gray-800 text-center md:text-left mb-6">
+        <h1 className="text-4xl m-4 font-bold text-gray-800 text-center sm:w-[95%] mb-8">
           Overview
         </h1>
         
 
-        <p className="text-gray-700 leading-relaxed text-lg text-center md:text-left">
+        <p className="text-gray-700 leading-relaxed text-lg text-justify sm:w-[95%]">
           Virtual Labs project is an initiative of <strong>Ministry of Education (MoE), Government of India</strong> under 
           the aegis of <strong>National Mission on Education through Information and Communication Technology (NMEICT)</strong>. 
           This project is a consortium activity of twelve participating institutes, with <strong>IIT Delhi</strong> as the 
@@ -26,12 +22,12 @@ const AboutUs = () => {
           for the first time.
         </p>
 
-        <p className="mt-6 text-gray-700 leading-relaxed text-lg text-center md:text-left">
+        <p className="mt-6 text-gray-700 leading-relaxed text-lg text-justify sm:w-[95%]">
           Under the Virtual Labs project, <strong>over 100 Virtual Labs</strong> consisting of approximately 
           <strong> 700+ web-enabled experiments</strong> were designed for remote-operation and viewing.
         </p>
 
-        <h2 className="text-2xl font-semibold text-gray-800 mt-8 mb-4 text-center md:text-left">
+        <h2 className="text-2xl font-semibold text-gray-800 mt-8 mb-4 text-justify sm:w-[95%]">
           The intended beneficiaries of the project are:
         </h2>
         <ul className="list-disc pl-6 text-gray-700 text-lg space-y-2">
@@ -41,7 +37,7 @@ const AboutUs = () => {
           <li>Engineering colleges benefiting from content and related teaching resources.</li>
         </ul>
 
-        <p className="mt-6 text-gray-700 leading-relaxed text-lg text-center md:text-left">
+        <p className="mt-6 text-gray-700 leading-relaxed text-lg text-justify sm:w-[95%]">
           <strong>Virtual Labs</strong> do not require any additional infrastructural setup for conducting experiments. 
           The simulation-based experiments can be accessed remotely via the internet, making high-quality education 
           more accessible.
